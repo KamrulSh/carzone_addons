b601f01cfef8d8a8a022ac78b27d593c0df6f70a
Kamrul Islam Shahin <shahin.cuetcse@gmail.com>
2024-04-02 07:20:08 +0600

    Modify garage booking module

--- rt_broadcast_mgmt/models/garage_booking.py
+++ rt_broadcast_mgmt/models/garage_booking.py
@@ -10,42 +10,60 @@ class GarageBooking(models.Model):
     customer_name = fields.Char(string='Customer Name', required=True)
     customer_email = fields.Char(string='Customer Email', required=True)
     customer_phone = fields.Char(string='Customer Phone', required=True)
+    booking_date = fields.Date(string='Booking Date')
     date_deadline = fields.Date(string='Date Deadline')
     planned_date = fields.Date(string='Planned Date')
-    vehicle_no = fields.Char(string='Vehicle No')
+    vehicle_no = fields.Char(string='Vehicle License No')
     vehicle_model = fields.Char(string='Vehicle Model')
     chassis_no = fields.Char(string='Chassis No')
-    state = fields.Selection([('draft', 'Draft'), ('confirm', 'Confirm'), ('broadcast', 'Broadcast')], string='Status',
-                             default='draft')
+    state = fields.Selection(
+        [('draft', 'Draft'), ('confirm', 'Confirm'), ('fleet', 'Fleet'), ('broadcast', 'Broadcast')],
+        string='Status', default='draft')
+    is_new_customer = fields.Char(string='Is New Customer', readonly=True, store=True)
 
     def action_confirm_booking(self):
         self.write({'state': 'confirm'})
-        vehicle_model = self.env['vehicle.model'].search([('name', '=', self.vehicle_model)], limit=1)
-        if not vehicle_model:
-            self.env['vehicle.model'].create({'name': self.vehicle_model})
-        vehicle_model_id = self.env['vehicle.model'].search([('name', '=', self.vehicle_model)], limit=1).id
-        customer_data = {
-            'name': self.customer_name,
-            'phone': self.customer_phone,
-            'email': self.customer_email,
-            'company_type': 'person',
-            'vehicle_no': self.vehicle_no,
-            'model_id': vehicle_model_id,
-            'chassis_no': self.chassis_no,
-        }
-        self.env['res.partner'].create(customer_data)
+        customer_vehicle = self.env['fleet.vehicle'].search([('license_plate', '=', self.vehicle_no)], limit=1)
+        if customer_vehicle:
+            self.is_new_customer = 'No'
 
-        return {
-            'type': 'ir.actions.client',
-            'tag': 'display_notification',
-            'sticky': True,
-            'params': {
-                'type': 'success',
-                'message': _('Booking confirmed and created customer data.'),
-                'next': {'type': 'ir.actions.act_window_close'},
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'sticky': True,
+                'params': {
+                    'type': 'warning',
+                    'message': _('This customer vehicle is already exist.'),
+                    'next': {'type': 'ir.actions.act_window_close'},
+                }
             }
-        }
 
+        else:
+            self.is_new_customer = 'Yes'
+            customer_data = {
+                'name': self.customer_name,
+                'phone': self.customer_phone,
+                'email': self.customer_email,
+                'company_type': 'person',
+            }
+            self.env['res.partner'].create(customer_data)
+
+            return {
+                'type': 'ir.actions.client',
+                'tag': 'display_notification',
+                'sticky': True,
+                'params': {
+                    'type': 'success',
+                    'message': _('Booking confirmed and created customer data.'),
+                    'next': {'type': 'ir.actions.act_window_close'},
+                }
+            }
+
+    # TODO: Implement this method
+    def action_create_fleet(self):
+        print('Creating')
+
+    # TODO: Modify this method
     def action_broadcast(self):
         self.write({'state': 'broadcast'})
         customer_id = self.env['res.partner'].search(
@@ -72,3 +90,7 @@ class GarageBooking(models.Model):
                 'next': {'type': 'ir.actions.act_window_close'},
             }
         }
+
+    # TODO: Implement this method
+    def action_send_message(self):
+        print('Sending message')
